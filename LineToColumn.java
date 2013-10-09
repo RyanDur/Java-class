db1a1d1bb0230342028ee9e057c6e8b60e46b181
RyanDur <ryan.p.durling@gmail.com>
2013-10-09 11:26:00 +0100

    Line to Column

---
+++ LineToColumn.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class LineToColumn {
+    public static void main(String []args) {
+        for(int i = 0; i < args[0].length(); i ++) {
+            System.out.println(args[0].charAt(i));
+        }
+    }
+}
