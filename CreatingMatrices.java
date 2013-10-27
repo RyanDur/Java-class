17d412c12c3e024187aba5890165bcca325c51c7
RyanDur <ryan.p.durling@gmail.com>
2013-10-27 15:33:24 +0000

    creating matrices

---
+++ CreatingMatrices.java
@@ -0,0 +1,16 @@
+public class CreatingMatrices {
+    public static void main(String []args) {
+        Matrix matrix = new Matrix(3,3);
+
+        System.out.println(matrix);
+
+        matrix.setRow(0,"1,2,3");
+        System.out.println(matrix);
+
+        matrix.setColumn(1, "4,5,6");
+        System.out.println(matrix);
+
+        matrix.setElement(2,2,10);
+        System.out.println(matrix);
+    }
+}
---
+++ Matrix.java
@@ -0,0 +1,64 @@
+public class Matrix {
+
+    private int[][] matrix;
+
+    public Matrix(int x,int y) {
+        matrix = new int[x][y];
+        initializeTo(1);
+    }
+
+    public void setElement(int x,int y, int value) {
+        if(valid(x, y)) {
+            matrix[x][y] = value;
+        }
+    }
+
+    public void setRow(int row, String list) {
+        String[] splitList = list.split(",");
+        if(valid(row, splitList.length-1)) {
+            for(int i = 0; i < matrix[row].length; i++) {
+                matrix[row][i] = Integer.parseInt(splitList[i]);
+            }
+        }
+    }
+
+    public void setColumn(int column, String list) {
+        String[] splitList = list.split(",");
+        if(valid(column, splitList.length-1)) {
+            for(int i = 0; i < matrix.length; i++) {
+                matrix[i][column] = Integer.parseInt(splitList[i]);
+            }
+        }
+    }
+
+    public String toString() {
+        String result = "[";
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[i].length; j++) {
+                if(i > 0 && j % matrix.length == 0) {
+                    result += ";";
+                }
+
+		if(j == matrix[i].length-1) {
+		    result += matrix[i][j];
+		} else {
+		    result += matrix[i][j] + ",";
+		}
+
+            }
+        }
+        return result + "]";
+    }
+
+    private boolean valid(int x, int y) {
+        return matrix.length > x && matrix[x].length > y;
+    }
+
+    private void initializeTo(int value) {
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = value;
+            }
+        }
+    }
+}
