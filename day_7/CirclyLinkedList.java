e3b859e70750a987d1a32f1dc77cb0eb6067e00d
RyanDur <ryan.p.durling@gmail.com>
2013-11-01 00:26:26 +0000

    Circly linked list

--- day_7/CirclyLinkedList.java
+++ day_7/CirclyLinkedList.java
@@ -1,7 +1,59 @@
 public class CirclyLinkedList {
+    private Node head = null;
+
+    public void add(Node node) {
+        if (head == null) {
+	    node.setNext(node);
+            head = node;
+        }
+        node.setNext(head.getNext());
+        head.setNext(node);
+    }
+
+    public void delete(Node node) {
+        if(head == node) {
+            head = node.getNext();
+        }
+        Node last = last(node, node.getNext());
+        last.setNext(node.getNext());
+        node.setNext(null);
+    }
+
+    public void printList() {
+        printEach(head, head);
+    }
+
+    public int size() {
+        return sumEach(head, head);
+    }
+
+    private Node last(Node head, Node next) {
+        if(next.getNext() == head) {
+            return next;
+        }
+        return last(head, next.getNext());
+    }
+
+    private void printEach(Node head, Node next) {
+        System.out.println(next.getNode());
+        if(next.getNext() == head) {
+            return;
+        }
+        printEach(head, next.getNext());
+    }
+
+    private int sumEach(Node head, Node next) {
+        if(head == null) {
+            return 0;
+        }
+        if(next.getNext() == head) {
+            return 1;
+        }
+        return 1 + sumEach(head, next.getNext());
+    }
 
     public static void main(String []args) {
-        CrclyLinkedList manager = new CrclyLinkedList();
+        CirclyLinkedList manager = new CirclyLinkedList();
         PatientNode patient1 = new PatientNode(new Patient("John", 33, "Tuberculosis"));
         PatientNode patient2 = new PatientNode(new Patient("Fliss", 21, "Video Game Addiction"));
         PatientNode patient3 = new PatientNode(new Patient("Keimi", 21, "Recursion"));
