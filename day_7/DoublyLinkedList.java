803ff6c5161a7035ab501a1ae0c5d35e78090a91
RyanDur <ryan.p.durling@gmail.com>
2013-11-01 00:31:13 +0000

    Boubly linked list

--- day_7/DoublyLinkedList.java
+++ day_7/DoublyLinkedList.java
@@ -1,7 +1,74 @@
 public class DoublyLinkedList {
+    private NodeDbl head = null;
+
+    public void add(NodeDbl node) {
+        if (head == null) {
+            head = node;
+        } else if(head.getNext() == null) {
+            head.setNext(node);
+            node.setLast(head);
+            resetHead();
+        } else {
+            head = head.getNext();
+            add(node);
+        }
+    }
+
+    public void delete(NodeDbl node) {
+        if(head == node) {
+            head = head.getNext();
+        }
+        if(node.getNext() != null) {
+            node.getNext().setLast(node.getLast());
+        }
+        if(node.getLast() != null) {
+            node.getLast().setNext(node.getNext());
+        }
+        node.setNext(null);
+        node.setLast(null);
+    }
+
+    public void printList() {
+        System.out.println(head.getNode());
+        if(head.getNext() == null) {
+	    System.out.println();
+            printLast();
+        } else {
+            head = head.getNext();
+            printList();
+        }
+    }
+
+    public int size() {
+        if(head == null) {
+            return 0;
+        }
+        if(head.getNext() == null) {
+            resetHead();
+            return 1;
+        }
+        head = head.getNext();
+        return 1 + size();
+    }
+
+    private void printLast() {
+	System.out.println(head.getNode());
+
+        if(head.getLast() != null) {
+	    head = head.getLast();
+            printLast();
+        }
+    }
+
+    private void resetHead() {
+        if(head.getLast() != null) {
+            head = head.getLast();
+            resetHead();
+        }
+    }
 
     public static void main(String []args) {
-        DblyLinkedList manager = new DblyLinkedList();
+        DoublyLinkedList manager = new DoublyLinkedList();
         PatientNodeDbl patient1 = new PatientNodeDbl(new Patient("John", 33, "Tuberculosis"));
         PatientNodeDbl patient2 = new PatientNodeDbl(new Patient("Fliss", 21, "Video Game Addiction"));
         PatientNodeDbl patient3 = new PatientNodeDbl(new Patient("Keimi", 21, "Recursion"));
