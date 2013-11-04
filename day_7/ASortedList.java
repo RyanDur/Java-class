bfa89e28e034bf3ccec8b2fcb1fd57036a36b930
RyanDur <ryan.p.durling@gmail.com>
2013-11-04 13:26:48 +0000

    A sorted List

--- day_7/ASortedList.java
+++ day_7/ASortedList.java
@@ -2,12 +2,12 @@ public class ASortedList {
 
     public static void main(String []args) {
         int [] ints = {5,2,3,6,15,1,9,20,19,7,14,4,16,18,8,10,15,11,12,13,17,1};
-	SortedLinkedList list = new SortedLinkedList();
+        SortedLinkedList list = new SortedLinkedList();
 
-	for(int i = 0; i < ints.length; i++) {
-	    list.insert(new IntegerNode(ints[i]));
-	}
+        for(int i = 0; i < ints.length; i++) {
+            list.add(new IntegerNode(ints[i]));
+        }
 
-	list.printList();
+        list.printList();
     }
 }
--- day_7/SortedLinkedList.java
+++ day_7/SortedLinkedList.java
@@ -1,36 +1,32 @@
 public class SortedLinkedList extends SinglyLinkedList {
 
     public void add(Node node) {
-	super.add(node);
+        if(start == null) {
+	    super.add(node);
+        } else if(start.compare(node)) {
+            node.setNext(start);
+            head = start = node;
+        } else if(head.getNext() == null){
+            super.add(node);
+        } else if(head.getNext().compare(node)) {
+            node.setNext(head.getNext());
+            head.setNext(node);
+            head = start;
+        } else {
+            head = head.getNext();
+            add(node);
+        }
     }
 
     public void delete(Node node) {
-	super.delete(node);
+        super.delete(node);
     }
 
     public void printList() {
-	super.printList();
+        super.printList();
     }
 
     public int size() {
-	return super.size();
-    }
-
-    public void insert(Node node) {
-	if(start == null) {
-	    add(node);
-	} else if(start.compare(node)) {
-	    node.setNext(start);
-	    head = start = node;
-	} else if(head.getNext() == null){
-	    add(node);
-	} else if(head.getNext().compare(node)) {
-	    node.setNext(head.getNext());
-	    head.setNext(node);
-	    head = start;
-	} else {
-	    head = head.getNext();
-	    insert(node);
-	}
+        return super.size();
     }
 }
