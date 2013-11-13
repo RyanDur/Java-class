7946f8048c9900bd41276598d736d012b73e1b91
RyanDur <ryan.p.durling@gmail.com>
2013-11-13 22:28:51 +0000

    remove Jdecaf and ad test

---
+++ day_9/IntegerBinaryTreeTest.java
@@ -0,0 +1,28 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: ryandurling
+ * Date: 11/13/13
+ * Time: 10:26 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class IntegerBinaryTreeTest {
+  @org.junit.Test
+  public void testAdd() throws Exception {
+
+  }
+
+  @org.junit.Test
+  public void testGetMin() throws Exception {
+
+  }
+
+  @org.junit.Test
+  public void testGetMax() throws Exception {
+
+  }
+
+  @org.junit.Test
+  public void testDepth() throws Exception {
+
+  }
+}
