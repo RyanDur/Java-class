767bfd09513caf310c0401e527ff89fcd0f91b17
RyanDur <ryan.p.durling@gmail.com>
2013-12-02 15:12:46 +0000

    Overriding

--- day_10/PhoneLauncher.java
+++ day_10/PhoneLauncher.java
@@ -4,9 +4,19 @@ public class PhoneLauncher {
         Phone p2 = new MobilePhone();
         Phone p3 = new SmartPhone();
 
+        System.out.println("Old Phone");
         p1.call("333333333");
+        p1.call("00333333333");
+
+        System.out.println();
+        System.out.println("Mobile Phone");
         p2.call("333333333");
-        p2.call("333333333");
+        p2.call("00333333333");
+
+        System.out.println();
+        System.out.println("Smart Phone");
+        p3.call("333333333");
+        p3.call("00333333333");
     }
 
     public static void main(String[] args) {
--- day_10/SmartPhone.java
+++ day_10/SmartPhone.java
@@ -1,22 +1,26 @@
 public class SmartPhone extends MobilePhone {
 
     public void call(String number) {
-	super.call(number);
+        if(number.matches("00.*")) {
+	    System.out.println("Calling " + number + " using the interwebs");
+        } else {
+            super.call(number);
+        }
     }
 
     public void ringAlarm(String alarm) {
-	super.ringAlarm(alarm);
+        super.ringAlarm(alarm);
     }
 
     public void playGame(String game) {
-	super.playGame(game);
+        super.playGame(game);
     }
 
     public void browseWeb(String search) {
-	System.out.println("Searching for " + search);
+        System.out.println("Searching for " + search);
     }
 
     public void findPosition() {
-	System.out.println("You are here.");
+        System.out.println("You are here.");
     }
 }
