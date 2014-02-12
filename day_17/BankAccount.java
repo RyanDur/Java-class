8978dfee1b5e431418bf267e55d5477865333290
RyanDur <ryan.p.durling@gmail.com>
2014-02-12 17:16:35 +0000

    bank account

---
+++ day_17/BankAccount.java
@@ -0,0 +1,20 @@
+public class BankAccount {
+    private int balance = 0;
+    public int getBalance() {
+        return balance;
+    }
+
+    public synchronized void deposit(int money) {
+        balance = balance + money;
+    }
+
+    public synchronized int retrieve(int money) {
+        int result = 0;
+        if (balance > money) {
+            result = money;
+        } else {
+            balance = balance - result;
+        }
+        return result;
+    }
+}
